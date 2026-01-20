7bbb6d68b337dc60c40051e1650793565960329a
juanma-alb <168273755+juanma-alb@users.noreply.github.com>
2026-01-20 14:26:36 -0300

    feat: add advanced stats helpers using Lodash and vanilla JS (ex 4.5-4.7)

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,4 @@
+const _ = require('lodash')
 const dummy = (_blogs) => {
   return 1
 }
@@ -14,7 +15,81 @@ const totalLikes = (blogs) => {
   return total
 }
 
+const maxLikes = (blogs) => {
+  let acc = 0
+  let obj = {}
+  blogs.forEach( blog =>
+  {
+
+    if (acc < blog.likes)
+    {acc = blog.likes
+      obj = blog}
+
+  }
+  )
+  return obj
+}
+
+const mostFavoritesBlog = (blogs) => {
+  let acc = 0
+  let obj = {}
+
+  blogs.forEach(blog => {
+
+    if (acc<blog.likes)
+    {
+      acc = blog.likes
+      obj= blog
+    }
+  })
+
+  return obj
+}
+
+const mostBlogs = (blogs) => {
+
+  const authorCounts = _.countBy(blogs, 'author')
+
+  const formattedList = _.map(authorCounts, (count, author) => {
+    return {
+      author: author,
+      blogs: count
+    }
+  })
+  return _.maxBy(formattedList, 'blogs')
+}
+
+const mostLikes = (blogs) => {
+  let obj = {}
+  blogs.forEach(blog => {
+
+    if (!obj[blog.author]){
+      obj[blog.author] = blog.likes
+    }
+    else {
+      obj[blog.author] += blog.likes
+    }
+  })
+
+  const authors = Object.entries(obj).map(([author, likes]) => {
+    return ({
+      author:author,
+      likes:likes
+    })
+  })
+  let mostLikes = 0
+  let mostLikesAuthor= {}
+
+  authors.forEach(author =>
+  {if (author.likes>mostLikes)
+  {mostLikesAuthor= author
+    mostLikes= author.likes
+  }}
+  )
+
+  return mostLikesAuthor
+}
 
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, maxLikes, mostFavoritesBlog, mostBlogs, mostLikes
 }
